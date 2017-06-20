0827257aed7cef67a8512f34448afe0cc3f7a7e8
aaksingh <deathakanote@gmail.com>
2017-06-20 05:07:38 -0700

    Street view of Grand Canyon

---
+++ app/src/main/java/com/example/ne/grandcanyon/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.ne.grandcanyon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.StreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
+
+public class MainActivity extends AppCompatActivity implements OnStreetViewPanoramaReadyCallback{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        StreetViewPanoramaFragment streetViewPanoramaFragment =
+                (StreetViewPanoramaFragment) getFragmentManager()
+                        .findFragmentById(R.id.streetviewpanorama);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
+
+    }
+    @Override
+    public void onStreetViewPanoramaReady(StreetViewPanorama panorama){
+        panorama.setPosition(new LatLng(36.0579667,-112.1430996));
+        StreetViewPanoramaCamera camera = new StreetViewPanoramaCamera.Builder()
+                .bearing(180)
+                .build();
+        panorama.animateTo(camera,10000);
+
+    }
+}
